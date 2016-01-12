011aa7d7675189b2e9c1ec14d2f6bc875a10bfa5
rekysyarifudin <reky@teamworks.co.id>
2016-01-12 14:51:55 +0700

    12/01/2016

--- database_tambahan/akad_xxx_all.sql
+++ database_tambahan/akad_xxx_all.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 12 Jan 2016 pada 13.02
+-- Generation Time: 12 Jan 2016 pada 14.51
 -- Versi Server: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -672,7 +672,7 @@ CREATE TABLE IF NOT EXISTS `akad_useraura` (
 --
 
 INSERT INTO `akad_useraura` (`UserId`, `user`, `password`, `email`, `avatar`, `level`, `tipe`, `is_online`, `last_ping`, `start`, `exp`, `biodata`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@elyon.sch.id', 'af0675a9e843c6c8f78163a9118efc78.jpg', 'Administrator', 'aktif', 1, '2016-01-12 09:41:24', '2010-08-27 00:00:00', '2034-08-27 00:00:00', '<p><b>none</b></p>'),
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@elyon.sch.id', 'af0675a9e843c6c8f78163a9118efc78.jpg', 'Administrator', 'aktif', 1, '2016-01-12 13:19:31', '2010-08-27 00:00:00', '2034-08-27 00:00:00', '<p><b>none</b></p>'),
 (28, 'superadmin', 'b11d5ece6353d17f85c5ad30e0a02360', 'rekysda@gmail.com', '', 'Administrator', 'aktif', 1, '2015-03-21 23:05:28', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- mod/kelas/admin/admin_kelas.php
+++ mod/kelas/admin/admin_kelas.php
@@ -11,14 +11,18 @@ if (!cek_login ()){
 $admin .='<p class="judul">Access Denied !!!!!!</p>';
 }else{
 
-$JS_SCRIPT= <<<js
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+$JS_SCRIPT.= <<<js
+
 <script type="text/javascript" src="js/chained/jquery.chained.min.js"></script>
 js;
 $JS_SCRIPT.= <<<js
-$("#series").chained("#mark"); /* or $("#series").chainedTo("#mark"); */
+<script language="JavaScript" type="text/javascript">
+$(function() {
+$("#tingkat").chained("#jenjang"); /* or $("#series").chainedTo("#mark"); */
+} );
+</script>
 js;
-/*
+
 $JS_SCRIPT .= <<<js
 <script language="JavaScript" type="text/javascript">
 $(document).ready(function() {
@@ -26,13 +30,76 @@ $(document).ready(function() {
 } );
 </script>
 js;
-*/
+
 
 $script_include[] = $JS_SCRIPT;
 $admin .='<h4 class="page-header">Administrasi Kelas</h4>';
 
 if($_GET['aksi']==""){
+	if (isset($_POST['lihatdata'])){
+	
+$lokasi = $_POST['lokasi'];
+$jenjang = $_POST['jenjang'];	
+$tingkat = $_POST['tingkat'];
+if($lokasi==''){
+         $wherelokasi="";
+}else{
+         $wherelokasi="where departemen='$lokasi'";
+}
+if($tingkat==''){
+         $wheretingkat="";
+}else{
+         $wheretingkat="and subtingkat='$tingkat'";
+}
+}
+$admin.='<form class="form-inline" method="post" action="" enctype ="multipart/form-data" id="posts">';
 $admin.='
+<table class="table">';
+$admin .= '<tr>
+	<td><select name="lokasi" class="form-control" id="lokasi"required>';
+$hasilj = $koneksi_db->sql_query("SELECT * FROM departemen ORDER BY urut asc");
+$admin .= '<option value="">== Lokasi ==</option>';
+while ($datasj =  $koneksi_db->sql_fetchrow ($hasilj)){
+		$pilihan = ($datasj['replid']==$lokasi)?"selected":'';
+$admin .= '<option value="'.$datasj['replid'].'"'.$pilihan.'>'.$datasj['nama'].'</option>';
+}
+$admin .='</select></td>';
+$admin .= '
+	<td><select name="jenjang" class="form-control" id="jenjang">';
+$hasilj = $koneksi_db->sql_query("SELECT * FROM aka_tingkat ORDER BY urutan asc");
+$admin .= '<option value="">== Jenjang ==</option>';
+while ($datasj =  $koneksi_db->sql_fetchrow ($hasilj)){
+		$pilihan = ($datasj['replid']==$jenjang)?"selected":'';
+$admin .= '<option value="'.$datasj['replid'].'"'.$pilihan.'>'.$datasj['tingkat'].'</option>';
+}
+$admin .='</select></td>';
+
+$admin .= '
+	<td><select name="tingkat" class="form-control" id="tingkat">';
+$hasilj = $koneksi_db->sql_query("SELECT * FROM aka_subtingkat ORDER BY subtingkat asc");
+$admin .= '<option value="">== Tingkat ==</option>';
+while ($datasj =  $koneksi_db->sql_fetchrow ($hasilj)){
+		$pilihan = ($datasj['replid']==$tingkat)?"selected":'';
+$admin .= '<option value="'.$datasj['replid'].'"'.$pilihan.'class="'.$datasj['tingkat'].'">'.$datasj['subtingkat'].'</option>';
+}
+$admin .='</select></td>';
+$admin .= '
+	<td><select name="tahunajaran" class="form-control" id="tahunajaran">';
+$hasilj = $koneksi_db->sql_query("SELECT * FROM aka_tahunajaran ORDER BY tahunajaran asc");
+$admin .= '<option value="">== Tahun Ajaran ==</option>';
+while ($datasj =  $koneksi_db->sql_fetchrow ($hasilj)){
+		$pilihan = ($datasj['replid']==$tingkat)?"selected":'';
+$admin .= '<option value="'.$datasj['replid'].'"'.$pilihan.'>'.$datasj['tahunajaran'].'</option>';
+}
+$admin .='</select></td>';
+$admin .= '
+		<td><input type="submit" value="Lihat" name="lihatdata" class="btn btn-success"></td>
+</tr>
+';
+$admin.='
+</table>';
+$admin .='</form>';
+$admin .= '
 <select id="mark" name="mark">
   <option value="">--</option>
   <option value="bmw">BMW</option>
@@ -48,11 +115,12 @@ $admin.='
   <option value="a5" class="audi">A5</option>
 </select>';
 /************************************/
+
 $admin.='
 <table id="example"class="table table-striped table-bordered" cellspacing="0" width="100%">
     <thead>
         <tr>
-            <th>Kelas</th>
+            <th>Nama Kelas</th>
             <th>Wali Kelas</th>
             <th>Kapasitas</th>
             <th>Terisi</th>
@@ -60,16 +128,19 @@ $admin.='
             <th>Aksi</th>
         </tr>
     </thead>';
-$hasil = $koneksi_db->sql_query( "SELECT * FROM akad_kelas order by id asc" );
+$hasil = $koneksi_db->sql_query( "SELECT * FROM akad_kelas $wherelokasi $wheretingkat order by replid asc" );
 while ($data = $koneksi_db->sql_fetchrow($hasil)) {
 $kelas=$data['kelas'];
-
+$walikelas=$data['walikelas'];
+$kapasitas=$data['kapasitas'];
+$replid=$data['replid'];
+$keterangan=$data['keterangan'];
 $admin .='<tr>
 <td>'.$kelas.'</td>
-<td>'.$kelas.'</td>
-<td>'.$kelas.'</td>
-<td>'.$kelas.'</td>
-<td>'.$kelas.'</td>
+<td>'.$walikelas.'</td>
+<td>'.$kapasitas.'</td>
+<td>'.$replid.'</td>
+<td>'.$keterangan.'</td>
 <td><a href="?pilih=kelas&amp;mod=yes&amp;aksi=del&amp;id='.$data['replid'].'" onclick="return confirm(\'Apakah Anda Yakin Ingin Menghapus Data Ini ?\')"><span class="btn btn-danger">Hapus</span></a> <a href="?pilih=kelas&amp;mod=yes&amp;aksi=edit&amp;id='.$data['replid'].'"><span class="btn btn-warning">Edit</span></a></td>
 </tr>';
 }
