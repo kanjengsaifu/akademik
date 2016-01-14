0e8215b5ad1e3b300666a520eab1d0a7db4ae98c
rekysyarifudin <reky@teamworks.co.id>
2016-01-14 08:33:56 +0700

    14/01/2016

--- database_tambahan/akad_xxx_all (minus akad_situs).sql
+++ database_tambahan/akad_xxx_all (minus akad_situs).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2016 at 08:33 AM
+-- Generation Time: Jan 14, 2016 at 08:32 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -350,40 +350,269 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `akad_situs`
+-- Table structure for table `akad_guru`
 --
 
-DROP TABLE IF EXISTS `akad_situs`;
-CREATE TABLE IF NOT EXISTS `akad_situs` (
-  `id` int(2) NOT NULL AUTO_INCREMENT,
-  `email_master` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `judul_situs` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `url_situs` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `slogan` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `description` text COLLATE latin1_general_ci NOT NULL,
-  `keywords` text COLLATE latin1_general_ci NOT NULL,
-  `maxkonten` int(2) NOT NULL DEFAULT '5',
-  `maxadmindata` int(2) NOT NULL DEFAULT '5',
-  `maxdata` int(2) NOT NULL DEFAULT '5',
-  `maxgalleri` int(2) NOT NULL DEFAULT '4',
-  `widgetshare` int(2) NOT NULL DEFAULT '0',
-  `theme` varchar(50) COLLATE latin1_general_ci NOT NULL DEFAULT 'tcms',
-  `author` text COLLATE latin1_general_ci NOT NULL,
-  `alamatkantor` text COLLATE latin1_general_ci NOT NULL,
-  `publishwebsite` int(1) NOT NULL DEFAULT '1',
-  `publishnews` int(2) NOT NULL,
-  `maxgalleridata` int(11) NOT NULL,
-  `widgetkomentar` int(2) NOT NULL DEFAULT '1',
-  `widgetpenulis` int(2) NOT NULL DEFAULT '2',
+DROP TABLE IF EXISTS `akad_guru`;
+CREATE TABLE IF NOT EXISTS `akad_guru` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `lokasi` varchar(50) NOT NULL,
+  `matpel` varchar(50) NOT NULL,
+  `guru` varchar(50) NOT NULL,
+  `sks` varchar(50) NOT NULL,
+  `status` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
--- Dumping data for table `akad_situs`
+-- Dumping data for table `akad_guru`
 --
 
-INSERT INTO `akad_situs` (`id`, `email_master`, `judul_situs`, `url_situs`, `slogan`, `description`, `keywords`, `maxkonten`, `maxadmindata`, `maxdata`, `maxgalleri`, `widgetshare`, `theme`, `author`, `alamatkantor`, `publishwebsite`, `publishnews`, `maxgalleridata`, `widgetkomentar`, `widgetpenulis`) VALUES
-(1, 'rekysda@gmail.com', 'Akademik', 'http://localhost/sister/akademik/', 'Akademik', 'WebDesign dengan sistem Responsive', 'akademik,student service,surabaya,indonesia', 5, 50, 5, 4, 3, 'pos', 'Elyon Christian School', 'Surabaya', 1, 1, 12, 1, 2);
+INSERT INTO `akad_guru` (`id`, `lokasi`, `matpel`, `guru`, `sks`, `status`) VALUES
+(1, '2', '4', '132', '11', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akad_jam`
+--
+
+DROP TABLE IF EXISTS `akad_jam`;
+CREATE TABLE IF NOT EXISTS `akad_jam` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) NOT NULL,
+  `jenjang` varchar(5) NOT NULL,
+  `mulai` varchar(10) NOT NULL,
+  `selesai` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `akad_jam`
+--
+
+INSERT INTO `akad_jam` (`id`, `nama`, `jenjang`, `mulai`, `selesai`) VALUES
+(1, '1', '1', '06.30', '07.15'),
+(2, '2', '1', '07.15', '08.00'),
+(3, '3', '1', '08.00', '08.45'),
+(4, '4', '1', '08.45', '09.30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akad_kalender`
+--
+
+DROP TABLE IF EXISTS `akad_kalender`;
+CREATE TABLE IF NOT EXISTS `akad_kalender` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `lokasi` varchar(4) NOT NULL,
+  `tgl1` datetime NOT NULL,
+  `tgl2` datetime NOT NULL,
+  `nama` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `akad_kalender`
+--
+
+INSERT INTO `akad_kalender` (`id`, `lokasi`, `tgl1`, `tgl2`, `nama`) VALUES
+(1, '2', '2016-01-01 08:00:00', '2016-01-08 11:00:00', 'Bersihkan kamar'),
+(6, '1', '2016-01-28 00:00:00', '2016-01-28 00:00:00', 'u6u'),
+(7, '1', '2016-01-05 00:00:00', '2016-01-07 00:00:00', 'test sukomanunggal'),
+(8, '4', '2016-01-14 00:00:00', '2016-01-22 00:00:00', 'ssf wrew rw werwer');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akad_kegiatan`
+--
+
+DROP TABLE IF EXISTS `akad_kegiatan`;
+CREATE TABLE IF NOT EXISTS `akad_kegiatan` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `matpel` varchar(50) NOT NULL,
+  `jenis` varchar(256) NOT NULL,
+  `penilaian` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `akad_kegiatan`
+--
+
+INSERT INTO `akad_kegiatan` (`id`, `matpel`, `jenis`, `penilaian`) VALUES
+(1, '5', 'wer wer', 'wer');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akad_kegiatannon`
+--
+
+DROP TABLE IF EXISTS `akad_kegiatannon`;
+CREATE TABLE IF NOT EXISTS `akad_kegiatannon` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(256) NOT NULL,
+  `sks` varchar(256) NOT NULL,
+  `thnajar` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akad_kelas`
+--
+
+DROP TABLE IF EXISTS `akad_kelas`;
+CREATE TABLE IF NOT EXISTS `akad_kelas` (
+  `replid` int(11) NOT NULL AUTO_INCREMENT,
+  `departemen` int(11) NOT NULL,
+  `kelas` varchar(100) NOT NULL,
+  `subtingkat` int(11) NOT NULL,
+  `kapasitas` int(10) unsigned NOT NULL DEFAULT '0',
+  `keterangan` text,
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `tahunajaran` int(5) NOT NULL,
+  `walikelas` int(5) NOT NULL,
+  `jenjang` int(5) NOT NULL,
+  PRIMARY KEY (`replid`),
+  KEY `departemenFK` (`departemen`),
+  KEY `subtingkatFK` (`subtingkat`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `akad_kelas`
+--
+
+INSERT INTO `akad_kelas` (`replid`, `departemen`, `kelas`, `subtingkat`, `kapasitas`, `keterangan`, `ts`, `tahunajaran`, `walikelas`, `jenjang`) VALUES
+(2, 1, 'ElyonSukoToodler1', 4, 20, 'ga ada', '2016-01-13 14:51:29', 12, 59, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akad_lessonplan`
+--
+
+DROP TABLE IF EXISTS `akad_lessonplan`;
+CREATE TABLE IF NOT EXISTS `akad_lessonplan` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `matpel` varchar(10) NOT NULL,
+  `tujuan` varchar(512) NOT NULL,
+  `target` varchar(215) NOT NULL,
+  `jangkawaktu` varchar(215) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akad_lomba`
+--
+
+DROP TABLE IF EXISTS `akad_lomba`;
+CREATE TABLE IF NOT EXISTS `akad_lomba` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(215) NOT NULL,
+  `pic` varchar(215) NOT NULL,
+  `bulan` varchar(215) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akad_matpel`
+--
+
+DROP TABLE IF EXISTS `akad_matpel`;
+CREATE TABLE IF NOT EXISTS `akad_matpel` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) NOT NULL,
+  `sks` varchar(10) NOT NULL,
+  `slot` varchar(10) NOT NULL,
+  `jenjang` varchar(10) NOT NULL,
+  `tingkat` varchar(10) NOT NULL,
+  `kuota` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `akad_matpel`
+--
+
+INSERT INTO `akad_matpel` (`id`, `nama`, `sks`, `slot`, `jenjang`, `tingkat`, `kuota`) VALUES
+(4, 'Matematika', '2', '10', '3', '1', '1'),
+(5, 'Bahasa Indonesia', '0', '2', '1', '1', '2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akad_pelanggaran`
+--
+
+DROP TABLE IF EXISTS `akad_pelanggaran`;
+CREATE TABLE IF NOT EXISTS `akad_pelanggaran` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(215) NOT NULL,
+  `point` varchar(215) NOT NULL,
+  `hukuman` varchar(512) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akad_ulangan`
+--
+
+DROP TABLE IF EXISTS `akad_ulangan`;
+CREATE TABLE IF NOT EXISTS `akad_ulangan` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) NOT NULL,
+  `kode` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `akad_ulangan`
+--
+
+INSERT INTO `akad_ulangan` (`id`, `nama`, `kode`) VALUES
+(1, 'Ulangan Harian 1', 'uh1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akad_useraura`
+--
+
+DROP TABLE IF EXISTS `akad_useraura`;
+CREATE TABLE IF NOT EXISTS `akad_useraura` (
+  `UserId` int(15) NOT NULL AUTO_INCREMENT,
+  `user` varchar(250) NOT NULL DEFAULT '',
+  `password` text NOT NULL,
+  `email` varchar(250) NOT NULL DEFAULT '',
+  `avatar` varchar(250) NOT NULL DEFAULT '',
+  `level` enum('Administrator','Penjualan','Kasir','Gudang','Accounting','Auditor') NOT NULL DEFAULT 'Administrator',
+  `tipe` varchar(250) NOT NULL DEFAULT '',
+  `is_online` int(5) NOT NULL DEFAULT '0',
+  `last_ping` text NOT NULL,
+  `start` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `exp` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `biodata` text NOT NULL,
+  PRIMARY KEY (`UserId`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
+
+--
+-- Dumping data for table `akad_useraura`
+--
+
+INSERT INTO `akad_useraura` (`UserId`, `user`, `password`, `email`, `avatar`, `level`, `tipe`, `is_online`, `last_ping`, `start`, `exp`, `biodata`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@elyon.sch.id', 'af0675a9e843c6c8f78163a9118efc78.jpg', 'Administrator', 'aktif', 1, '2016-01-14 08:13:32', '2010-08-27 00:00:00', '2034-08-27 00:00:00', '<p><b>none</b></p>'),
+(28, 'superadmin', 'b11d5ece6353d17f85c5ad30e0a02360', 'rekysda@gmail.com', '', 'Administrator', 'aktif', 1, '2015-03-21 23:05:28', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
