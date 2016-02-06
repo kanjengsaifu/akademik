d50ece4d73c88ce9f3cfd3f8f5d2ed508f58406a
rekysyarifudin <reky@teamworks.co.id>
2016-02-06 13:47:22 +0700

    06/02/2016

--- database_tambahan/departemen_jenjang.sql
+++ database_tambahan/departemen_jenjang.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 06 Feb 2016 pada 13.43
+-- Generation Time: 06 Feb 2016 pada 13.46
 -- Versi Server: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -20,35 +20,11 @@ SET time_zone = "+00:00";
 -- Database: `sister_siadu`
 --
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `departemen`
---
-
-DROP TABLE IF EXISTS `departemen`;
-CREATE TABLE IF NOT EXISTS `departemen` (
-  `replid` int(11) NOT NULL AUTO_INCREMENT,
-  `nama` varchar(50) NOT NULL,
-  `kepsek` int(10) unsigned NOT NULL DEFAULT '0',
-  `urut` int(10) unsigned NOT NULL DEFAULT '1',
-  `keterangan` varchar(255) NOT NULL,
-  `alamat` varchar(300) NOT NULL,
-  `telepon` varchar(30) NOT NULL,
-  `photo` blob NOT NULL,
-  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`replid`),
-  UNIQUE KEY `UX_departemen_replid` (`replid`),
-  UNIQUE KEY `departemen` (`nama`),
-  KEY `FK_departemen_pegawai` (`kepsek`),
-  KEY `IX_departemen_ts` (`ts`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
-
 --
 -- Dumping data untuk tabel `departemen`
 --
 
-INSERT INTO `departemen` (`replid`, `nama`, `kepsek`, `urut`, `keterangan`, `alamat`, `telepon`, `photo`, `ts`) VALUES
+REPLACE INTO `departemen` (`replid`, `nama`, `kepsek`, `urut`, `keterangan`, `alamat`, `telepon`, `photo`, `ts`) VALUES
 (1, 'Elyon Sukomanunggal', 98, 1, '1', 'Jl. Raya Sukomanunggal Jaya 33A', '(031)732-5999', '', '2014-01-21 16:50:40'),
 (2, 'Elyon Rungkut', 98, 2, '1', 'Ruko Rungkut Megah Raya A-25, Jl. Raya Kali Rungkut No. 5', '(031)879-8896', '', '2014-01-23 19:14:27'),
 (3, 'Elyon Kertajaya', 98, 3, '6', 'Jl. Kertajaya Indah Timur VII/41', '(031)599-4994', '', '2014-01-23 19:14:34');
